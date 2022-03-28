f4f9d163600f2f99adb9a9408b123dd54b2ef65d
Giuseppe Lobraico <giuseppe.lobraico@digital.justice.gov.uk>
2022-03-28 16:06:46 +0100

    Use node 16 and bump dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './assets/css/App.css';
 import Dashboard from './pages/Dashboard';
@@ -8,14 +8,10 @@ import Admin from './pages/Admin';
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path='/'>
-          <Dashboard />
-        </Route>
-        <Route path='/admin'>
-          <Admin />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path='/' element={<Dashboard />}></Route>
+        <Route path='/admin' element={<Admin />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 };
